8c6a1a8fbe35c9f4d9168f95672e34f07758f28f
kaosfang <kaosfang@tutanota.com>
2021-12-26 23:28:44 -0600

    Finished install_gamer.rb

--- install_gamer.rb
+++ install_gamer.rb
@@ -4,6 +4,7 @@
 This script will install the programs needed for gamers
   - Game Launchers  : Steam or Lutris
   - Communication   : Discord or Matrix
+  - EXTRAS          : 
 
 =end
 puts "First installing an AUR Helper and updating and syncing repos"
@@ -11,7 +12,11 @@ system("sudo pacman -S paru")
 sleep 2
 system("sudo pacman -Syuu")
 system("clear")
-puts "[type the name of the program in all lower case] (recommended)steam or lutris"
+puts "Now we will begin installing important programs for gaming on linux"
+system("sudo pacman -S wine")
+system("paru -S dxvk-bin")
+system('clear')
+print "[type the name of the program in all lower case] (recommended)steam or lutris: "
 gamelauncher = gets.chomp.to_s
 
 if gamelauncher == "steam"
@@ -19,3 +24,15 @@ if gamelauncher == "steam"
 elsif gamelauncher == "lutris"
   system('sudo pacman -S lutris')
 end
+
+print "[type the name of the program in all lower case] discord or matrix: "
+comms = gets.chomp.to_s
+
+if comms == "discord"
+  system('sudo pacman -S discord')
+elsif comms == "matrix"
+  system('sudo pacman -S element-desktop')
+end
+
+system('clear')
+puts "Installed programs\n -#{gamelauncher}\n -#{comms}\n -wine\n -dxvk "
